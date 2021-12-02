49fe71d03a7990d5b51d2a105e74e6b4ef0fe313
Wei Ding <ding_web@yahoo.co.jp>
2021-12-01 17:48:40 -0800

    add duration

--- Backend/IsoAnalyzer/IsoParser.Lib/Concretes/Parser.cs
+++ Backend/IsoAnalyzer/IsoParser.Lib/Concretes/Parser.cs
@@ -14,7 +14,6 @@ namespace IsoParser.Lib.Concretes {
 		private long fileSize;
 
 		private readonly HashSet<AtomType> containers;
-		private readonly HashSet<AtomType> leaves;
 
 		#region movie variable
 		private int? timeScale;
@@ -110,67 +109,60 @@ namespace IsoParser.Lib.Concretes {
 			if (atom.Type.HasValue && !this.isContainer ((AtomType)atom.Type))
 				atom.Items = this.Parse (atom);
 
-			//if (atom.Type.HasValue || id == 1) {
-			//	if (this.isContainer ((AtomType)atom.Type)) {
+			List<Atom> atoms = new ();
+			bool valid = true;
 
-					List<Atom> atoms = new ();
-					bool valid = true;
+			long si;
+			for (long ip = offset + head; ip < offset + size - head; ip += si) {
+				byte [] buffer = this.file.Read (8, ip);
+				if (buffer.Length < 1) {
+					valid = false;
+					break;
+				}
 
-					long si;
-					for (long ip = offset + head; ip < offset + size - head; ip += si) {
-						byte [] buffer = this.file.Read (8, ip);
+				int atomId = ByteInt (buffer, 4);
+				int atomHead = 8;
+
+				if (this.ValidId (atomId)) {
+					//				if (this.valid) {
+					//					if (this.valid && this.ValidId (atomId)) {
+					switch (si = this.ByteInt (buffer, 0)) {
+					case 0:
+						si = this.fileSize - ip;
+						break;
+					case 1:
+						buffer = this.file.Read (8);
 						if (buffer.Length < 1) {
 							valid = false;
 							break;
-						}
-
-						int atomId = ByteInt (buffer, 4);
-						int atomHead = 8;
-
-						if (this.ValidId (atomId)) {
-							//				if (this.valid) {
-							//					if (this.valid && this.ValidId (atomId)) {
-							switch (si = this.ByteInt (buffer, 0)) {
-							case 0:
-								si = this.fileSize - ip;
-								break;
-							case 1:
-								buffer = this.file.Read (8);
-								if (buffer.Length < 1) {
-									valid = false;
-									break;
-								} else {
-									si = this.ByteLong (buffer, 0);
-									atomHead += 8;
-								}
-								break;
-							}
-
 						} else {
-							atomId = 0;
-							atomHead = 0;
-							si = size - ip;
-							valid = false;
+							si = this.ByteLong (buffer, 0);
+							atomHead += 8;
 						}
-
-						Console.WriteLine ($"  atomId {atomId:x}, si {si:x}, ip {ip:x}, atomHead {atomHead:x}");
-
-						if (Enum.IsDefined (typeof (AtomType), atomId)) {
-							Console.WriteLine ($"-- Found Atom [{(AtomType)atomId}]");
-							Atom newAtom = GetAtom (atomId, si, ip, atomHead);
-							atoms.Add (newAtom);
-						}
-
-						if (!valid)
-							break;
+						break;
 					}
 
-					if (atoms.Count > 0)
-						atom.Atoms = atoms;
-			//	} else {
-			//		//atom.Items = this.Parse (atom);
-			//	}
-			//}
+				} else {
+					atomId = 0;
+					atomHead = 0;
+					si = size - ip;
+					valid = false;
+				}
+
+				Console.WriteLine ($"  atomId {atomId:x}, si {si:x}, ip {ip:x}, atomHead {atomHead:x}");
+
+				if (Enum.IsDefined (typeof (AtomType), atomId)) {
+					Console.WriteLine ($"-- Found Atom [{(AtomType)atomId}]");
+					Atom newAtom = GetAtom (atomId, si, ip, atomHead);
+					atoms.Add (newAtom);
+				}
+
+				if (!valid)
+					break;
+			}
+
+			if (atoms.Count > 0)
+				atom.Atoms = atoms;
 
 			return atom;
 		}
@@ -208,13 +200,22 @@ namespace IsoParser.Lib.Concretes {
 			byte [] buffer = this.file.Read ((int)atom.Size, atom.Offset);
 			if(buffer.Length >= (int)atom.Size) {
 				List<Item> items = new ();
+
 				int count = ByteInt (buffer, 12);
 				items.Add (new Item { Name = "Entries", Type = ItemType.Int, Value = count });
+
 				for(int i = 0; i < count; i++) {
 					items.Add (new Item { Name = "TrackDuration", Type = ItemType.Int, Value = ByteInt (buffer, 16 + 12 * i) });
 					items.Add (new Item { Name = "MediaTime", Type = ItemType.Int, Value = ByteInt (buffer, 20 + 12 * i) });
 					items.Add (new Item { Name = "MediaRate", Type = ItemType.Int, Value = ByteInt (buffer, 24 + 12 * i) });
+					if (this.timeScale.HasValue && this.timeScale != 0)
+						items.Add (new Item {
+							Name = "DurationSec",
+							Type = ItemType.Double,
+							Value = (double)ByteInt (buffer, 16 + 12 * i) / (double)this.timeScale
+						});
 				}
+
 				return items;
 			}
 			return Array.Empty <Item> ().ToList ();

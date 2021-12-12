195f3adcb60af60910a63cdffca73471d3043768
Wei Ding <ding_web@yahoo.co.jp>
2021-12-11 23:46:27 -0800

    add iso controller

---
+++ Backend/IsoAnalyzer/IsoParser.Api/Controllers/IsoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using IsoParser.Lib.Concretes;
+using IsoParser.Lib.Models;
+using IsoParser.Lib.Services;
+
+namespace IsoParser.Api.Controllers
+{
+    [ApiController]
+    [Route ("[controller]")]
+    public class IsoController : ControllerBase
+    {
+        private readonly ILogger<Parser> _logger;
+        private readonly IParser _parser;
+
+        public IsoController (ILogger<Parser> logger, IParser parser)
+        {
+            this._logger = logger;
+            this._parser = parser;
+
+        }
+
+        [HttpGet]
+        public string Get ()
+        {
+            return "Returning a parsing result";
+        }
+    }
+}

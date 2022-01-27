620b33851eee59f35e0126da1d22dc84f01a5895
Wei Ding <ding_web@yahoo.co.jp>
2022-01-27 00:12:57 -0800

    add subtitle attributes

--- Backend/IsoAnalyzer/IsoParser.Lib/Models/Subtitle.cs
+++ Backend/IsoAnalyzer/IsoParser.Lib/Models/Subtitle.cs
@@ -4,6 +4,9 @@ namespace IsoParser.Lib.Models
 {
     public class Subtitle
     {
+        public string Format { get; set; }
+        public string Type { get; set; }
         public List<string> Boxes { get; set; }
+        public List<string> Notes { get; set; }
     }
 }

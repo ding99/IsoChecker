09839eb4ebfacdbf050d3a1b0450a68bffaace97
Wei Ding <ding_web@yahoo.co.jp>
2022-02-09 23:15:14 -0800

    diaplay less

--- Backend/IsoAnalyzer/CheckIso/Worker.cs
+++ Backend/IsoAnalyzer/CheckIso/Worker.cs
@@ -9,6 +9,7 @@ using Parsers.MccParser;
 
 namespace CheckIso {
 	public class Worker {
+		private const int maxRepeat = 5;
 		private readonly MccParser mcc;
 
 		public Worker ()
@@ -220,10 +221,10 @@ namespace CheckIso {
                     dsp = true;
                 }
 
-                if (dsp)
-                    b.Append (data).Append (Environment.NewLine);
-                if (count == 6)
-                    b.Append ("......").Append (Environment.NewLine);
+                //if (dsp)
+                //    b.Append (data).Append (Environment.NewLine);
+                //if (count == maxRepeat)
+                //    b.Append ("......").Append (Environment.NewLine);
 			}
 
 			return b.ToString ();

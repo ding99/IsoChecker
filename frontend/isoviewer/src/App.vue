61dbdb77c169c0608ac1de8ae02144c9e2771dd5
Wei Ding <ding_web@yahoo.co.jp>
2022-04-02 00:00:10 -0700

    change root

--- frontend/isoviewer/src/App.vue
+++ frontend/isoviewer/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Iso Viewer"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloIso.vue'
 
 export default {
   name: 'App',

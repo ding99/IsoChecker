4d3c2bb713c3f844a0df0f06b2f9148688dd77d4
Wei Ding <ding_web@yahoo.co.jp>
2022-04-17 11:23:18 -0700

    atom comment

--- Backend/IsoAnalyzer/IsoParser.Lib/Models/Atom.cs
+++ Backend/IsoAnalyzer/IsoParser.Lib/Models/Atom.cs
@@ -57,7 +57,7 @@ namespace IsoParser.Lib.Models
 		clip = 0x636c6970,  //clip, clipping
 		cmov = 0x636d6f76,  //cmov, compressed movie
 		colr = 0x636f6c72,  //colr, color parameters
-		crgn = 0x6372676e,  //crgn
+		crgn = 0x6372676e,  //crgn, clipping region
 		crsr = 0x63727372,  //crsr, sprite cursor behavior
 		cslg = 0x63736c67,  //cslg, composition shift least greatest
 		ctab = 0x63746162,  //ctab, color table
@@ -66,14 +66,14 @@ namespace IsoParser.Lib.Models
 		data = 0x64617461,  //data, value atome
 		defi = 0x64656669,  //defi, sprite image defualt image index
 		dflt = 0x64666c74,  //dflt, sprite shared data
-		dinf = 0x64696e66,  //dinf
-		dref = 0x64726566,  //dref
+		dinf = 0x64696e66,  //dinf, data info
+		dref = 0x64726566,  //dref, data ref
 		drpo = 0x6472706f,  //drpo, text sample
 		drpt = 0x64727074,  //drpt, text sample
-		edts = 0x65647473,  //edts
+		edts = 0x65647473,  //edts, edit
 		elng = 0x656c6e67,  //elng, extended language tag
-		elst = 0x656c7374,  //elst
-		enof = 0x656e6f66,  //enof
+		elst = 0x656c7374,  //elst, edit list
+		enof = 0x656e6f66,  //enof, track encoded pixels demension
 		fall = 0x66616c6c,  //fall, identical content audio trck
 		fiel = 0x6669656c,  //fiel, field handling, two 8-bit integers
 		flov = 0x666c6f76,  //flov, sprite floating point variable
@@ -81,7 +81,7 @@ namespace IsoParser.Lib.Models
 		forc = 0x666f7263,  //forc, forced subtitle
 		free = 0x66726565,  //free, free space
 		ftab = 0x67746162,  //ftab, text sample
-		ftyp = 0x66747970,  //ftyp
+		ftyp = 0x66747970,  //ftyp, file type compatibility
 		gmhd = 0x676d6864,  //gmhd, base media info head
 		gmin = 0x676d696e,  //gmin, base media info
 
@@ -92,12 +92,12 @@ namespace IsoParser.Lib.Models
 		hlit = 0x686c6974,  //hlit, text sample
 		hnti = 0x686e7469,  //hnti, movie user data
 		htxt = 0x68747874,  //htxt, hypertext
-		idat = 0x69646174,  //idat
-		idsc = 0x69647363,  //idsc
-		ilst = 0x696c7374,  //ilst
+		idat = 0x69646174,  //idat, image data
+		idsc = 0x69647363,  //idsc, image description
+		ilst = 0x696c7374,  //ilst, metadata item list
 		imda = 0x696d6461,  //imda, sprite imaged datatkhd
 		imag = 0x696d6167,  //imag, text sample, sprite image
-		imap = 0x696d6170,  //imap
+		imap = 0x696d6170,  //imap, track input map
 		imct = 0x696d6374,  //imct, sprite images container
 		imgr = 0x696d6772,  //imgr, sprite image group id
 		imre = 0x696d7265,  //imre, sprite image data ref
@@ -105,36 +105,36 @@ namespace IsoParser.Lib.Models
 		imrt = 0x696d7274,  //imrt, sprite image data ref type
 		itif = 0x69746966,  //itif, item information
 		keys = 0x6b657973,  //keys, metadata item keys
-		kmat = 0x6b6d6174,  //kmat
+		kmat = 0x6b6d6174,  //kmat, compressed metta
 		lang = 0x6c616e67,  //lang, language list
 		load = 0x6c6f6164,  //load, track load settings
-		matt = 0x6d617474,  //matt
-		mdat = 0x6d646174,  //mdat
-		mdia = 0x6d646961,  //mdia
-		mdhd = 0x6d646864,  //mdhd
+		matt = 0x6d617474,  //matt, track metta
+		mdat = 0x6d646174,  //mdat, media data
+		mdia = 0x6d646961,  //mdia, media
+		mdhd = 0x6d646864,  //mdhd, media header
 		mdta = 0x6d647461,  //mdta, key namespace
-		meta = 0x6d657461,  //meta
+		meta = 0x6d657461,  //meta, metadata
 		metr = 0x6d657472,  //metr, text sample
-		mhdr = 0x6d686472,  //mhdr
+		mhdr = 0x6d686472,  //mhdr, metadata header
 		minf = 0x6d696e66,  //minf, media info
-		moov = 0x6d6f6f76,  //moov
-		mvhd = 0x6d766864,  //mvhd
+		moov = 0x6d6f6f76,  //moov, movie
+		mvhd = 0x6d766864,  //mvhd, movie header
 		name = 0x6e616d65,  //name, sprite name, image name
 
-		obid = 0x6f626964,  //obid
+		obid = 0x6f626964,  //obid, object id
 		pasp = 0x70617370,  //pasp, pixel aspect ratio
 		pnot = 0x706e6f74,  //pnot, preview atom
-		prfl = 0x7072666c,  //prfl
-		prof = 0x70726f66,  //prof
-		rmda = 0x726d6461,  //rmda
+		prfl = 0x7072666c,  //prfl, profile
+		prof = 0x70726f66,  //prof, track production aperture dimensions
+		rmda = 0x726d6461,  //rmda, reference movie descriptor
 		rmra = 0x726d7261,  //rmra, ref movie
 		rsrc = 0x72737363,  //rsrc, Macintosh alias
 		sbgp = 0x73626770,  //sbgp, sample-to-group
 		scpt = 0x73637074,  //scpt, transcript (ref a text track)
 		sdtp = 0x73647470,  //sdtp, sample dependency flags
 		sgpd = 0x73677064,  //sgpd, sample group description
-		skip = 0x736b6970,  //skip
-		smhd = 0x736d6864,  //smhd
+		skip = 0x736b6970,  //skip, placeholder for unused space
+		smhd = 0x736d6864,  //smhd, sound media info header
 		sprt = 0x73707274,  //sprt, sprite
 		ssrc = 0x73737263,  //ssrc, none primary source
 		sstr = 0x73737472,  //sstr, sprite status string behavior
@@ -150,19 +150,19 @@ namespace IsoParser.Lib.Models
 		stts = 0x73747473,  //stts, time-to-sample
 		styl = 0x7374796c,  //styl, text sample
 		sync = 0x73796e63,  //sync, synchronization
-		tapt = 0x74617074,  //tapt
+		tapt = 0x74617074,  //tapt, track apeture mode dimensions
 		tcmi = 0x74636d69,  //tcmi, timecode media info
-		tkhd = 0x746b6864,  //tkhd
+		tkhd = 0x746b6864,  //tkhd, track header
 		tmcd = 0x746d6364,  //tmcd, time code
 		tnam = 0x746e616d,  //tnam, track name
-		trak = 0x7472616b,  //trak
+		trak = 0x7472616b,  //trak, track
 		tref = 0x74726566,  //tref, track reference
-		txas = 0x74786173,  //txas
+		txas = 0x74786173,  //txas, track exclude from autoselection
 
 		udta = 0x75647461,  //udta, user data
 		uses = 0x75736573,  //uses, spite uses image id
 		vars = 0x76617273,  //vars, sprite variable container
-		vmhd = 0x766d6864,  //vmhd
+		vmhd = 0x766d6864,  //vmhd, video media info header
 		wide = 0x77696465,  //wide, placeholder
 		wtxt = 0x77747874,  //wtxt, wired text
 
@@ -173,18 +173,18 @@ namespace IsoParser.Lib.Models
 		TY = 0x20207479,  // '  ty'
 
 		#region self define
-		TOP = 0x746f7020,   // 'top ', top level of mov file
-		ITEM = 0x6974656d,  // item, metadata item correponding to key_index
+		TOP = 0x544f5020,   // 'top ', top level of mov file
+		ITEM = 0x4954454d,  // item, metadata item correponding to key_index
 		#endregion
 
 		#region codec
-		avc1 = 0x61766331,  //avc1
+		avc1 = 0x61766331,  //avc1, H.264 video
 		avcC = 0x61766343,  //avcC, H.264 Configuration Box, 14496-15
 
-		mp4a = 0x6d703461,  //mp4a
+		mp4a = 0x6d703461,  //mp4a, MPEG-4, advanced audio coding (AAC)
 
-		c608 = 0x63363038,  //c608
-		c708 = 0x63373038,  //c708
+		c608 = 0x63363038,  //c608, closed caption format
+		c708 = 0x63373038,  //c708, closed caption format
 		#endregion
 	}
 	public enum GraphicsMode

f61749fbf44c4a11ca8177b9f560ff30155be0ff
Wei Ding <ding_web@yahoo.co.jp>
2022-06-08 22:37:13 -0700

    nav bar

--- frontend/isoviewer/src/App.vue
+++ frontend/isoviewer/src/App.vue
@@ -1,6 +1,15 @@
 <template>
-  <img alt="iso viewer logo" src="./assets/iso32.png">
-  <ContainerRoot msg="Iso Viewer"/>
+  <v-app id="app">
+    <v-app-bar dark app heigth="48px">
+      <div class="navbar">
+        <img alt="iso viewer logo" src="./assets/iso32.png">
+        <div class="ml-3">Iso Viewer</div>
+      </div>
+    </v-app-bar>
+    <v-main>
+      <ContainerRoot msg="Iso Viewer"/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
@@ -14,13 +23,9 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scaped>
+.navbar {
+  display: flex;
+  flex-direction: row;
 }
 </style>
